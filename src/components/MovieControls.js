288b1085135dc95cf9c35e3b7562e5ab2436796c
Mayra Jaimes <mtj26@georgetown.edu>
2020-10-03 16:50:15 -0500

    first commit

---
+++ src/components/MovieControls.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export const MovieControls = ({ movie, type }) => {
+  const {
+    removeMovieFromWatchlist,
+    addMovieToWatched,
+    removeMovieFromWatched,
+    moveMovieBackToWatchlist,
+  } = useContext(GlobalContext);
+  return (
+    <div className="inner-card-controls">
+      {type === "watchlist" && (
+        <React.Fragment>
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+            <i className="fa-fw far fa-eye"></i>
+          </button>
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieFromWatchlist(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </React.Fragment>
+      )}
+      {type === "watched" && (
+        <React.Fragment>
+          <button
+            className="ctrl-btn"
+            onClick={() => moveMovieBackToWatchlist(movie)}
+          >
+            <i className="fa-fw far fa-eye-slash"></i>
+          </button>
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieFromWatched(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
